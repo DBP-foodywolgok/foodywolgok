e36763ad06c2c583fe4276b020d6396fd3488b5e
kimjiheej <glp0509@naver.com>
2023-12-02 22:10:41 +0900

    DAO CRUD 추가

--- src/main/java/model/dao/CustomerDAO.java
+++ src/main/java/model/dao/CustomerDAO.java
@@ -18,6 +18,7 @@ public class CustomerDAO {
 		jdbcUtil = new JDBCUtil();	// JDBCUtil 객체 생성
 	}
 	
+	// 회원 가입 
 	public int createCustomer(Customer customer) throws SQLException {
 	    String sql = "INSERT INTO Customer VALUES (customer_seq.nextval,?, ?, ?)";
 	    Object[] param = new Object[] { customer.getEmail(), customer.getPassword(), customer.getName() };
@@ -35,45 +36,200 @@ public class CustomerDAO {
 	    return 0;
 	}
 	
-	// 테이블에 새로운 사용자 정보 수정
-	
-	public int update(Customer customer) throws SQLException {
-		String sql = "UPDATE Customer "
-					+ "SET customer_id=?, email=?, password=?, name=? "
-					+ "WHERE customer_id=?";
-		Object[] param = new Object[] {customer.getCustomer_id(), customer.getEmail(), 
-					customer.getPassword(), customer.getName()};			
-		jdbcUtil.setSqlAndParameters(sql, param);	// JDBCUtil에 update문과 매개 변수 설정
-		try {				
-			int result = jdbcUtil.executeUpdate();	// update 문 실행
-			return result;
-		} catch (Exception ex) {
-			jdbcUtil.rollback();
-			ex.printStackTrace();
-		}
-		finally {
-			jdbcUtil.commit();
-			jdbcUtil.close();	// resource 반환
-		}		
-		return 0;
+	// 회원 이름으로 아이디 가져오기 
+	public int getCustomerIdByName(String customerName) throws SQLException {
+	    int customerId = 0;
+	    String sql = "SELECT customer_id FROM Customer WHERE name = ?";
+	    jdbcUtil.setSqlAndParameters(sql, new Object[]{customerName});
+
+	    try {
+	        ResultSet rs = jdbcUtil.executeQuery();
+	        if (rs.next()) {
+	            customerId = rs.getInt("customer_id");
+	        }
+	        return customerId;
+	    } catch (Exception ex) {
+	        ex.printStackTrace();
+	    } finally {
+	        jdbcUtil.close();
+	    }
+	    return customerId;
 	}
 	
-	// 사용자 정보 삭제 
 	
-	public int remove(String customerId) throws SQLException {
-		String sql = "DELETE FROM Customer WHERE customer_id=?";		
-		jdbcUtil.setSqlAndParameters(sql, new Object[] {customerId});	// JDBCUtil에 delete문과 매개 변수 설정
-		try {				
-			int result = jdbcUtil.executeUpdate();	// delete 문 실행
-			return result;
-		} catch (Exception ex) {
-			jdbcUtil.rollback();
-			ex.printStackTrace();
-		}
-		finally {
-			jdbcUtil.commit();
-			jdbcUtil.close();	// resource 반환
-		}		
-		return 0;
+	
+	// 회원정보 수정 
+	   public int update(Customer customer, String email) throws SQLException {
+	        String sql = "UPDATE Customer SET email=?, name=? WHERE email=?";
+	        Object[] param = new Object[]{customer.getEmail(), customer.getName(), email};
+	        jdbcUtil.setSqlAndParameters(sql, param); // JDBCUtil에 update문과 매개 변수 설정
+
+	        try {
+	            int result = jdbcUtil.executeUpdate(); // update 문 실행
+	            return result;
+	        } catch (Exception ex) {
+	            jdbcUtil.rollback();
+	            ex.printStackTrace();
+	        } finally {
+	            jdbcUtil.commit();
+	            jdbcUtil.close(); // resource 반환
+	        }
+	        return 0;
+	    }
+	
+	
+	// 로그인 구현 
+	public boolean login(String email, String password) throws SQLException {
+	    boolean isValid = false;
+	    String sql = "SELECT * FROM Customer WHERE email = ? AND password = ?";
+	    jdbcUtil.setSqlAndParameters(sql, new Object[]{email, password});
+
+	    try {
+	        ResultSet rs = jdbcUtil.executeQuery();
+	        if (rs.next()) {
+	            isValid = true;
+	        }
+	    } catch (Exception ex) {
+	    	jdbcUtil.rollback();
+	        ex.printStackTrace();
+	    } finally {
+	    	jdbcUtil.commit();
+	        jdbcUtil.close();
+	    }
+	    return isValid;
 	}
+	
+	// 비밀번호 찾기 
+	public String findPasswordByEmail(String email) throws SQLException {
+	    String password = null;
+	    String sql = "SELECT password FROM Customer WHERE email = ?";
+	    jdbcUtil.setSqlAndParameters(sql, new Object[]{email});
+
+	    try {
+	        ResultSet rs = jdbcUtil.executeQuery();
+	        if (rs.next()) {
+	            password = rs.getString("password");
+	        }
+	    } catch (Exception ex) {
+	        ex.printStackTrace();
+	    } finally {
+	        jdbcUtil.close();
+	    }
+	    return password;
+	}
+	
+	// 회원정보 조회 
+	public Customer getCustomerByEmail(String email) throws SQLException {
+	    Customer customer = null;
+	    String sql = "SELECT * FROM Customer WHERE email = ?";
+	    jdbcUtil.setSqlAndParameters(sql, new Object[]{email});
+
+	    try {
+	        ResultSet rs = jdbcUtil.executeQuery();
+	        if (rs.next()) {
+	            customer = new Customer();
+	            customer.setCustomer_id(rs.getInt("customer_id"));
+	            customer.setEmail(rs.getString("email"));
+	            customer.setPassword(rs.getString("password"));
+	            customer.setName(rs.getString("name"));
+	        }
+	    } catch (Exception ex) {
+	        ex.printStackTrace();
+	    } finally {
+	        jdbcUtil.close();
+	    }
+	    return customer;
+	}
+	
+	// 비밀번호 변경 
+	
+	 public int updatePassword(int customerId, String newPassword) throws SQLException {
+	        String sql = "UPDATE Customer SET password=? WHERE customer_id=?";
+	        Object[] param = new Object[]{newPassword, customerId};
+	        jdbcUtil.setSqlAndParameters(sql, param); // JDBCUtil에 update문과 매개 변수 설정
+
+	        try {
+	            int result = jdbcUtil.executeUpdate(); // update 문 실행
+	            return result;
+	        } catch (Exception ex) {
+	            jdbcUtil.rollback();
+	            ex.printStackTrace();
+	        } finally {
+	            jdbcUtil.commit();
+	            jdbcUtil.close(); // resource 반환
+	        }
+	        return 0;
+	    }
+	 
+	 // 친구 추가 
+	    public boolean addFriend(int customerId1, int customerId2) throws SQLException {
+	        boolean isSuccess = false;
+	        String sql = "INSERT INTO FriendList VALUES (?, ?)";
+	        
+	        jdbcUtil.setSqlAndParameters(sql, new Object[]{customerId1, customerId2});
+	        
+	        try {
+	            int result = jdbcUtil.executeUpdate();
+	            if (result > 0) {
+	                isSuccess = true;
+	            }
+	        } catch (Exception ex) {
+	            jdbcUtil.rollback();
+	            ex.printStackTrace();
+	        } finally {
+	            jdbcUtil.commit();
+	            jdbcUtil.close();
+	        }
+	        return isSuccess;
+	    }
+	    
+	    // 친구 조회 
+
+
+public List<Integer> getFriendList(int customerId) throws SQLException {
+    List<Integer> friendList = new ArrayList<>();
+    String sql = "SELECT friend_id FROM FriendList WHERE cust_id = ?";
+    
+    jdbcUtil.setSqlAndParameters(sql, new Object[]{customerId});
+    
+    try {
+        ResultSet rs = jdbcUtil.executeQuery();
+        while (rs.next()) {
+            int friendId = rs.getInt("friend_id");
+            friendList.add(friendId);
+        }
+    } catch (Exception ex) {
+        jdbcUtil.rollback();
+        ex.printStackTrace();
+    } finally {
+    	jdbcUtil.commit();
+        jdbcUtil.close();
+    }
+    return friendList;
 }
+
+// 친구 삭제 
+public boolean removeFriend(int customerId1, int customerId2) throws SQLException {
+    boolean isSuccess = false;
+    String sql = "DELETE FROM FriendList WHERE (cust_id = ? AND friend_id = ?) OR (cust_id = ? AND friend_id = ?)";
+    
+    jdbcUtil.setSqlAndParameters(sql, new Object[]{customerId1, customerId2, customerId2, customerId1});
+    
+    try {
+        int result = jdbcUtil.executeUpdate();
+        if (result > 0) {
+            isSuccess = true;
+        }
+    } catch (Exception ex) {
+        jdbcUtil.rollback();
+        ex.printStackTrace();
+    } finally {
+        jdbcUtil.commit();
+        jdbcUtil.close();
+    }
+    return isSuccess;
+}
+	
+  }
+	
+
--- src/main/java/test/CustomerTest.java
+++ src/main/java/test/CustomerTest.java
@@ -13,71 +13,68 @@ public class CustomerTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		// 고객 회원가입 
-//		Customer customer = new Customer();
-//	
-//		    
-//	        customer.setCustomer_id(1); // 고객 ID 설정 (고유한 값)
-//	        customer.setEmail("1234@naver.com"); // 이메일 설정
-//	        customer.setPassword("password123"); // 비밀번호 설정
-//	        customer.setName("Jen"); // 이름 설정
-//	        
-//	        List<String> selectedCategories = Arrays.asList("한식", "양식", "중식");
-//	        customer.setFavoriteCategories(selectedCategories); // 고객이 선택한 카테고리 정보 설정
+		// 회원가입 
+//	    Customer customer = new Customer();
 //
-//	        // CustomerDAO 인스턴스 생성
-	        CustomerDAO customerDAO = new CustomerDAO();
+//        customer.setEmail("mn2302@naver.com");
+//        customer.setPassword("password12345");
+//        customer.setName("Min");
 //
-//	        try {
-//	            int result = customerDAO.createCustomer(customer); // 고객 추가
+//        List<String> selectedCategories = Arrays.asList("한식", "양식", "중식");
+//        customer.setFavoriteCategories(selectedCategories);
 //
-//	            if (result > 0) {
-//	                System.out.println("Customer 정보가 성공적으로 추가되었습니다.");
+//        CustomerDAO customerDAO = new CustomerDAO();
+//        CategoryDAO categoryDAO = new CategoryDAO();
 //
-//	                // 고객이 선택한 카테고리 정보를 customer_Category 테이블에 추가
-//	                for (String category : selectedCategories) {
-//	                    int categoryId = getCategoryIDByName(category); // 카테고리 이름으로부터 ID를 가져옴 (메서드 구현 필요)
-//	                    int customerId = customer.getCustomer_id(); // 추가한 고객의 ID 가져옴
+//        try {
+//            int result = customerDAO.createCustomer(customer); // 고객 추가
+//            int customerId = customerDAO.getCustomerIdByName(customer.getName());
+//          
+//            if (result > 0) {
+//                System.out.println("Customer 정보가 성공적으로 추가되었습니다.");
+//              
+//                for (String category : selectedCategories) {
+//                    int categoryId = categoryDAO.getCategoryIdByName(category); // 카테고리 ID 가져오기
+//                     System.out.println(categoryId);
+//                    // Customer_Category 테이블에 추가
+//                    int categoryResult = categoryDAO.addCustomerCategory(categoryId, customerId);
 //
-//	                    // Customer_Category 테이블에 추가
-//	                    int categoryResult = customerDAO.addCustomerCategory(categoryId, customerId);
-//
-//	                    if (categoryResult > 0) {
-//	                        System.out.println("Customer_Category 정보가 성공적으로 추가되었습니다.");
-//	                    } else {
-//	                        System.out.println("Customer_Category 정보 추가에 실패했습니다.");
-//	                    }
-//	                }
-//	            } else {
-//	                System.out.println("Customer 정보 추가에 실패했습니다.");
-//	            }
-//	        } catch (SQLException e) {
-//	            e.printStackTrace();
-//	        }
-//	
-//	
-//	    
-	Customer customer2 = new Customer();
+//                    if (categoryResult > 0) {
+//                        System.out.println("Customer_Category 정보가 성공적으로 추가되었습니다.");
+//                    } else {
+//                        System.out.println("Customer_Category 정보 추가에 실패했습니다.");
+//                    }
+//                }
+//            } else {
+//                System.out.println("Customer 정보 추가에 실패했습니다.");
+//            }
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//    
 	
-    
-    customer2.setCustomer_id(1); // 고객 ID 설정 (고유한 값)
-    customer2.setEmail("1234@naver.com"); // 이메일 설정
-    customer2.setPassword("password123"); // 비밀번호 설정
-    customer2.setName("Jimin"); // 이름 설정
-
-    // CustomerDAO 인스턴스 생성
-
-    try {
-        // CustomerDAO를 사용하여 생성된 고객 정보를 DB에 추가
-        int result = customerDAO.createCustomer(customer2);
-        if (result > 0) {
-            System.out.println("Customer 정보가 성공적으로 추가되었습니다.");
-        } else {
-            System.out.println("Customer 정보 추가에 실패했습니다.");
-        }
-    } catch (SQLException e) {
-        e.printStackTrace();
-    }
+//	    
+//	Customer customer2 = new Customer();
+//	
+//    
+//    customer2.setCustomer_id(1); // 고객 ID 설정 (고유한 값)
+//    customer2.setEmail("1234@naver.com"); // 이메일 설정
+//    customer2.setPassword("password123"); // 비밀번호 설정
+//    customer2.setName("Jimin"); // 이름 설정
+//
+//    // CustomerDAO 인스턴스 생성
+//
+//    try {
+//        // CustomerDAO를 사용하여 생성된 고객 정보를 DB에 추가
+//        int result = customerDAO.createCustomer(customer2);
+//        if (result > 0) {
+//            System.out.println("Customer 정보가 성공적으로 추가되었습니다.");
+//        } else {
+//            System.out.println("Customer 정보 추가에 실패했습니다.");
+//        }
+//    } catch (SQLException e) {
+//        e.printStackTrace();
+//    }
 //
 //
 //
@@ -103,8 +100,158 @@ public class CustomerTest {
 //
 //    // 
 //}
+	
+	  // 
+	
+	// 로그인 
+		  CustomerDAO customerDAO = new CustomerDAO();
+//	    String inputEmail = "glp0509@naver.com";
+//	    String inputPassword = "password123";
+//
+//	    try {
+//	        boolean isLoggedIn = customerDAO.login(inputEmail, inputPassword);
+//	        if (isLoggedIn) {
+//	            System.out.println("로그인이 완료되었습니다.");
+//	        } else {
+//	            System.out.println("정보를 잘못 입력하셨습니다.");
+//	        }
+//	    } catch (SQLException e) {
+//	        e.printStackTrace();
+//	    }
+//	    
+//	    // 비밀번호 찾기 
+//	
+//
+//	    try {
+//	        String foundPassword = customerDAO.findPasswordByEmail(inputEmail);
+//	        if (foundPassword != null) {
+//	            System.out.println("찾은 비밀번호: " + foundPassword);
+//	        } else {
+//	            System.out.println("해당 이메일을 가진 사용자가 없습니다.");
+//	        }
+//	    } catch (SQLException e) {
+//	        e.printStackTrace();
+//	    }
+//	    
+//	    // 회원 정보 조회 
+//	    try {
+//            // 이메일을 기반으로 회원 정보 조회
+//            Customer customerInfo = customerDAO.getCustomerByEmail(inputEmail);
+//
+//            if (customerInfo != null) {
+//                // 조회된 회원 정보 출력
+//                System.out.println("회원 정보:");
+//                System.out.println("ID: " + customerInfo.getCustomer_id());
+//                System.out.println("Email: " + customerInfo.getEmail());
+//                System.out.println("Password: " + customerInfo.getPassword());
+//                System.out.println("Name: " + customerInfo.getName());
+//            } else {
+//                System.out.println("해당 이메일을 가진 사용자가 없습니다.");
+//            }
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//    
+//	    
+	  // 회원 정보 수정 
+	    Customer customerToUpdate = new Customer();
+        customerToUpdate.setCustomer_id(1); // 수정할 회원의 고유한 ID 설정
+        customerToUpdate.setEmail("swiftie0509@gmail.com");
+        customerToUpdate.setName("june");
+        String email = "glp0509@naver.com";
+        try {
+            
+            int result = customerDAO.update(customerToUpdate,email);
+
+            if (result > 0) {
+                System.out.println("회원 정보가 성공적으로 수정되었습니다.");
+            } else {
+                System.out.println("회원 정보 수정에 실패했습니다.");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    
+	
+	// 비밀번호 변경 
+	   int customerId = 8; // 해당 고객의 ID
+       String newPassword = "newPassword123"; // 새로운 비밀번호
+
+       try {
+           int result = customerDAO.updatePassword(customerId, newPassword);
+
+           if (result > 0) {
+               System.out.println("비밀번호가 성공적으로 변경되었습니다.");
+           } else {
+               System.out.println("비밀번호 변경에 실패했습니다.");
+           }
+       } catch (SQLException e) {
+           e.printStackTrace();
+       }
+       
+       // 친구 추가 
+       // 친구 추가 
+ 	  int customerId1 = 15; // 첫 번째 고객 ID
+       int customerId2 = 8; // 두 번째 고객 ID
+
+       try {
+           boolean result = customerDAO.addFriend(customerId1, customerId2);
+
+           if (result) {
+               System.out.println("친구 추가가 완료되었습니다.");
+           } else {
+               System.out.println("친구 추가에 실패했습니다.");
+           }
+       } catch (SQLException e) {
+           e.printStackTrace();
+       }
+//	
+   
+	
+	// 친구 조회 
+    try {
+        // customerId가 1인 고객의 친구 목록 조회
+        int customer_Id = 15;
+        System.out.println("CustomerId가 " + customer_Id + "인 고객의 친구 목록:");
+
+        // 특정 고객의 친구 목록 가져오기
+        List<Integer> friendList = customerDAO.getFriendList(customer_Id);
+
+        if (friendList.isEmpty()) {
+            System.out.println("친구가 없습니다.");
+        } else {
+            for (int friendId : friendList) {
+                System.out.println("친구 ID: " + friendId);
+            }
+        }
+    } catch (SQLException e) {
+        e.printStackTrace();
+    }
+    
+  // 친구 삭제 
+//    int customerId1 = 15; // 첫 번째 사용자 ID
+//    int customerId2 = 8; // 두 번째 사용자 ID
+//
+//    try {
+//        boolean isRemoved = customerDAO.removeFriend(customerId1, customerId2);
+//
+//        if (isRemoved) {
+//            System.out.println("친구 관계 삭제 성공");
+//        } else {
+//            System.out.println("친구 관계 삭제 실패");
+//        }
+//    } catch (SQLException e) {
+//        e.printStackTrace();
+//    }
+//}
+//    
+    
 }
 }
+  
+
+
+
 
 
 
