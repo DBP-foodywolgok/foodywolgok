73ed8623d531bd1a4385d6ad4f2f17504a9f0c1d
ymkdev <eveminful@gmail.com>
2023-12-05 00:12:34 +0900

    [Update] DiaryTest, DiaryDAO
    1. create(Diary diary) 수정
    - Sequence를 통해 생성한 PK값을 구하는 코드 추가 -> DiaryTest code에 이용
    2. update(Diary diary, int diary_id)
    - 파라미터 추가

--- src/main/java/model/dao/DiaryDAO.java
+++ src/main/java/model/dao/DiaryDAO.java
@@ -15,22 +15,33 @@ public class DiaryDAO {
 		jdbcUtil = new JDBCUtil(); // JDBCUtil 객체 생성
 	}
 	/**
-	 * 다이어리 새로운 글 생성
+	* 다이어리 새로운 글 생성
 	 */
 	public int create(Diary diary) throws SQLException {
 		String sql = "INSERT INTO DIARY VALUES (diary_seq.nextval, ?, ?, TO_DATE(SYSDATE, 'YYYY-MM-DD'), ?, ?, ?, ?)";
 		Blob photoBlob = (diary.getPhoto() != null) ? diary.getPhoto() : null;
 		// 파라미터 값 가져오기 //
+		
 		Object[] param = new Object[] {diary.getTitle(),
 				diary.getIsShared(), photoBlob, diary.getContent(),
 				diary.getRestaurant_id(), diary.getCustomer_id()};
-		
+	
 		jdbcUtil.setSqlAndParameters(sql, param); // JDBCUtil 에 insert문과 매개 변수 설정
+		
+		String key[] = {"diary_id"};
 		try {
-			int result = jdbcUtil.executeUpdate(); // insert 문 실행
-			return result;
+			int result = jdbcUtil.executeUpdate(key); // insert 문 실행
+		   	ResultSet rs = jdbcUtil.getGeneratedKeys();
+		   	int generatedKey = 0;
+		   	if(rs.next()) {
+		   		generatedKey = rs.getInt(1);     //  PK 컬럼 값(들)을 읽음
+		   		diary.setDiary_id(generatedKey); 	// id필드에 저장  
+		   		  
+		   	}
+		   	return result;
+	
 		} catch (Exception ex) {
-			System.out.println("bb");
+			//System.out.println("오류");
 			jdbcUtil.rollback();
 			ex.printStackTrace();
 		} finally {
@@ -39,35 +50,19 @@ public class DiaryDAO {
 		}
 		return 0;
 	}
+	
 	/**
+	 * 
 	 * 기존의 다이어리를 수정. */
-	public int update(Diary diary) throws SQLException {
-		String sql = "UPDATE DIARY " + "SET title=?, isshared=?, photo=?, content=? " + "WHERE diary_id=?";
+	public int update(Diary diary, int diary_id) throws SQLException {
+		String sql = "UPDATE DIARY " + "SET title=?, isshared=?, photo=?, content=? " + "WHERE diary_id= ?";
 		Blob photoBlob = (diary.getPhoto() != null) ? diary.getPhoto() : null;
 		Object[] param = new Object[] {diary.getTitle(), diary.getIsShared(),
-				photoBlob, diary.getContent()};
+				photoBlob, diary.getContent(), diary_id};
 		jdbcUtil.setSqlAndParameters(sql, param); // JDBCUtil에 update문과 매개 변수 설정
 		try {
 			int result = jdbcUtil.executeUpdate(); // update 문 실행
 			return result;
-		} catch (Exception ex) {
-			jdbcUtil.rollback();
-			ex.printStackTrace();
-		}
-		finally {
-			jdbcUtil.commit();
-			jdbcUtil.close(); // resource 반환
-		}
-		return 0;
-	}
-	/**
-	 * 다이어리 ID에 해당하는 다이어리를 삭제. */
-	public int remove(int diary_id) throws SQLException {
-		String sql = "DELETE FROM DIARY WHERE diary_id=?";
-		jdbcUtil.setSqlAndParameters(sql, new Object[] {diary_id}); //JDBCUtil에 delete문과 매개 변수 설정
-		try {
-			int result = jdbcUtil.executeUpdate(); // delete 문 실행
-			return result;	
 		} catch (Exception ex) {
 			jdbcUtil.rollback();
 			ex.printStackTrace();
--- src/main/java/test/DiaryTest.java
+++ src/main/java/test/DiaryTest.java
@@ -8,26 +8,70 @@ import model.dao.DiaryDAO;
 
 public class DiaryTest {
     
-    // 다이어리 등록
+    
     public static void main(String[] args) {
     	
-        Diary diary = new Diary(1, "오늘은 제나키친을 갔다", 1, null, null,
-                "완전 맛있었음. 김치찜 강추!! 다음에 오면 다른거 먹어봐야겠다~~", 1, 8);
+    	//다이어리 등록 
+       
+        Diary diary1= new Diary();
+        diary1.setTitle("오늘은 제나키친을 갔다.");
+        diary1.setIsShared(1);
+        diary1.setContent("완전 맛있었음 김치찜 강추~~ 다음에 오면 다른거 먹어봐야겠다");
+        diary1.setCustomer_id(8);
+        diary1.setRestaurant_id(1);
         
+        Diary diary2 = new Diary();
+        diary2.setTitle("오늘 저녁에 국수나무에 갔다!!");
+        diary2.setIsShared(0);
+        diary2.setContent("음 돈까스는 그저 그런..... 뭐를 먹지 다음엔");
+        diary2.setCustomer_id(8);
+        diary2.setRestaurant_id(1);
+        Diary[] diarys= {diary1, diary2};
+        		
         DiaryDAO diaryDAO = new DiaryDAO();
         
         try {
-            int result;
-            result = diaryDAO.create(diary);
+        	for(Diary diary : diarys) {
+        	
+                int result = diaryDAO.create(diary);
+                
+                if (result > 0) {
+                    System.out.println("다이어리 글 추가 완료" +diary);
+
+                }
+                else
+                    System.out.println("다이어리 글 추가 실패"+diary);
+        		
+        	}
             
-            if (result > 0)
-                System.out.println("다이어리 글 추가 완료" + diary);
-            else
-                System.out.println("다이어리 글 추가 실패" + diary);
 
         } catch (SQLException e) {
             e.printStackTrace();
         }
+        
+        //다이어리 수정 
+        
+        DiaryDAO dao = new DiaryDAO();
+		Diary diary2ToUpdate = new Diary();
+		diary2ToUpdate.setTitle("국수나무 돈까스 클리어~");
+		diary2ToUpdate.setIsShared(1);
+		diary2ToUpdate.setContent("음.. 사실 맛있게 먹었음ㅎㅎ 돈까스도 강추");
+		
+		try {
+			//System.out.println(diary2.getDiary_id());
+			int result = dao.update(diary2ToUpdate, diary2.getDiary_id());
+			
+			if (result > 0) {
+				System.out.println("다이어리 글 수정 완료");
+			} else {
+				System.out.println("다이어리 글 수정 실패");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+        
     }
+    
+    
 }
 
