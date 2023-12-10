0a942e808e7e547e4b51eddc592f415647beaf2a
Jieun Kim <imfunctionx@gamil.com>
2023-12-10 21:49:38 +0900

    [UPDATE] My Restaurant 등록 페이지 수정

---
+++ src/main/java/controller/myrestaurant/RegisterMyRestaurantController.java
@@ -0,0 +1,17 @@
+package controller.myrestaurant;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import controller.Controller;
+import controller.customer.UserSessionUtils;
+
+public class RegisterMyRestaurantController implements Controller {
+
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		return null;
+	}
+}
